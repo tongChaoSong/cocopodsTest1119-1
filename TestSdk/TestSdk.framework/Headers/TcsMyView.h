8a1ce940a8d206a75c47f18a23dc33020aa6ddb3
青春丶不过如此 <447154692@qq.com>
2020-11-19 16:05:16 +0800

    tcs

---
+++ TestSdk/TestSdk.framework/Headers/TcsMyView.h
@@ -0,0 +1,17 @@
+//
+//  TcsMyView.h
+//  TestSdk
+//
+//  Created by Apple on 2020/11/18.
+//  Copyright © 2020 tcs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TcsMyView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestSdk/TestSdk.framework/Headers/TestSdk.h
@@ -0,0 +1,19 @@
+//
+//  TestSdk.h
+//  TestSdk
+//
+//  Created by Apple on 2020/11/18.
+//  Copyright © 2020 tcs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestSdk.
+FOUNDATION_EXPORT double TestSdkVersionNumber;
+
+//! Project version string for TestSdk.
+FOUNDATION_EXPORT const unsigned char TestSdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestSdk/PublicHeader.h>
+
+#import <TestSdk/TcsMyView.h>
